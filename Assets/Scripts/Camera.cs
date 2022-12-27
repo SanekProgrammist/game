6a0fbfa370c7daa4b33d3a7d877e9152f36f2666
SanekProgrammist <sanek1325476@gmail.com>
2022-12-27 17:43:20 +1000

    Camera following...

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform following;
+  
+    void Update()
+    {
+        transform.position = new Vector3(following.position.x,
+                                         transform.position.y, transform.position.z);
+    }
+}
