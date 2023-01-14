6a674ccf8f5d80b06d25f98bc065995cd7072072
SanekProgrammist <120445055+SanekProgrammist@users.noreply.github.com>
2023-01-14 12:15:07 +0300

    Fall_from_Level...

---
+++ Assets/Scripts/NewGame2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame2 : MonoBehaviour
+{
+    public int location7;
+
+    public void NewGame3()
+    {
+        SceneManager.LoadScene(location7);
+    }
+}
