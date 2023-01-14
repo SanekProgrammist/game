3d9b1bf969741914420022498fc7860b604b955f
SanekProgrammist <120445055+SanekProgrammist@users.noreply.github.com>
2023-01-14 12:43:42 +0300

    End_Of_The_Game...

---
+++ Assets/Scripts/NewGame5.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame5 : MonoBehaviour
+{
+    public int location8;
+
+    public void NewGame4()
+    {
+        SceneManager.LoadScene(location8);
+    }
+}
